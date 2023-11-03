6885c43532619fd9b977d0a4d1f18c661ba1341a
Junebeomstics <76743210+Junebeomstics@users.noreply.github.com>
2023-11-03 18:20:15 +0900

    Update preprocessing.py

--- project/module/utils/data_preprocess_and_load/preprocessing.py
+++ project/module/utils/data_preprocess_and_load/preprocessing.py
@@ -4,24 +4,29 @@ import os
 import time
 from multiprocessing import Process, Queue
 
-def read_abcd(dir,load_root,save_root,count,queue=None,scaling_method=None, fill_zeroback=False):
-    print("processing: " + dir, flush=True)
-    path = os.path.join(load_root, dir)
+def read_data(filename,load_root,save_root,subj_name,count,queue=None,scaling_method=None, fill_zeroback=False):
+    print("processing: " + filename, flush=True)
+    path = os.path.join(load_root, filename)
     try:
+        # load each nifti file
         data, meta = LoadImage()(path)
     except:
         return None
     
     #change this line according to your file names
-    save_dir = os.path.join(save_root,dir[14:-7])
+    save_dir = os.path.join(save_root,subj_name)
     isExist = os.path.exists(save_dir)
     if not isExist:
         os.makedirs(save_dir)
     
     # change this line according to your dataset
-    data = data[1:-2, 14:-7, :, 20:] #It was once [10:-10, 10:-10,0:-10,20:] 
-    # it should be 96, 97, 95
-    # please crop at the end of y dim by 1, and pad 1 to z dim to make dimension 96 * 96 * 96
+    data = data[:, 14:-7, :, :]
+    # width, height, depth, time
+    # Inspect the fMRI file first using your visualization tool. 
+    # Limit the ranges of width, height, and depth to be under 96. Crop the background, not the brain regions. 
+    # Each dimension of fMRI registered to MNI space (2mm) is expected to be around 100.
+    # You can do this when you load each volume at the Dataset class, including padding backgrounds to fill dimensions under 96.
+   
     background = data==0
     
     if scaling_method == 'z-norm':
@@ -33,11 +38,10 @@ def read_abcd(dir,load_root,save_root,count,queue=None,scaling_method=None, fill
 
     data_global = torch.empty(data.shape)
     data_global[background] = data_temp[~background].min() if not fill_zeroback else 0 
-    # data_temp[~background].min() is expected to be ..
-    # 0 for scaling_method == 'minmax'
-    # minimum z-value for scaling_method == 'z-norm'
+    # data_temp[~background].min() is expected to be 0 for scaling_method == 'minmax', and minimum z-value for scaling_method == 'z-norm'
     data_global[~background] = data_temp[~background]
 
+    # save volumes one-by-one in fp16 format.
     data_global = data_global.type(torch.float16)
     data_global_split = torch.split(data_global, 1, 3)
     for i, TR in enumerate(data_global_split):
@@ -46,29 +50,37 @@ def read_abcd(dir,load_root,save_root,count,queue=None,scaling_method=None, fill
 
 def main():
     # change two lines below according to your dataset
-    load_root = '/storage/bigdata/ABCD/fmriprep/1.rs_fmri/4.cleaned_image'
-    save_root = '/storage/bigdata/ABCD/fmriprep/1.rs_fmri/7.ABCD_MNI_to_TRs_minmax'
-    scaling_method = 'z-norm' #'minmax'
+    dataset_name = 'ABCD'
+    load_root = '/storage/4.cleaned_image' # This folder should have fMRI files in nifti format with subject names. Ex) sub-01.nii.gz 
+    save_root = f'/storage/7.{dataset_name}_MNI_to_TRs_minmax'
+    scaling_method = 'z-norm' # choose either 'z-norm'(default) or 'minmax'.
 
-    dirs = os.listdir(load_root)
+    # make result folders
+    filenames = os.listdir(load_root)
     os.makedirs(os.path.join(save_root,'img'), exist_ok = True)
-    os.makedirs(os.path.join(save_root,'metadata'), exist_ok = True)
+    os.makedirs(os.path.join(save_root,'metadata'), exist_ok = True) # locate your metadata file at this folder 
     save_root = os.path.join(save_root,'img')
     
     finished_samples = os.listdir(save_root)
     queue = Queue() 
     count = 0
-    for dir in sorted(dirs):
+    for filename in sorted(filenames):
+        subj_name = filename[:-7] 
+        # extract subject name from nifti file. [:-7] rules out '.nii.gz'
+        # we recommend you use subj_name that aligns with the subject key in a metadata file.
+
+        expected_seq_length = 1000 # Specify the expected sequence length of fMRI for the case your preprocessing stopped unexpectedly and you try to resume the preprocessing.
+        
         # change the line below according to your folder structure
-        if (dir[14:-7] not in finished_samples) or (len(os.listdir(os.path.join(save_root,dir[14:-7]))) < 353):
+        if (subj_name not in finished_samples) or (len(os.listdir(os.path.join(save_root,subj_name))) < expected_seq_length): # preprocess if the subject folder does not exist, or the number of pth files is lower than expected sequence length. 
             try:
                 count+=1
-                p = Process(target=read_abcd, args=(dir,load_root,save_root,count,queue,scaling_method))
+                p = Process(target=read_data, args=(filename,load_root,save_root,subj_name,count,queue,scaling_method))
                 p.start()
                 if count % 32 == 0: # requires more than 32 cpu cores for parallel processing
                     p.join()
             except Exception:
-                print('encountered problem with'+dir)
+                print('encountered problem with'+filename)
                 print(Exception)
 
 if __name__=='__main__':
